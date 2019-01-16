3f36c4d3f79b638eb06c01548f60ba03043e4aee
Bernhard Paus Græsdal <bernhardpg@gmail.com>
2019-01-16 22:22:43 +0100

    Create channel test

--- Part2/go/foo.go
+++ Part2/go/foo.go
@@ -3,8 +3,8 @@
 package main
 
 import (
-    . "fmt"
-    "runtime"
+	. "fmt"
+	"runtime"
 )
 
 // Control signals
@@ -19,21 +19,21 @@ func number_server(add_number <-chan int, control <-chan int, number chan<- int)
 	// This for-select pattern is one you will become familiar with if you're using go "correctly".
 	for {
 		select {
-			// TODO: receive different messages and handle them correctly
-			// You will at least need to update the number and handle control signals.
+		// TODO: receive different messages and handle them correctly
+		// You will at least need to update the number and handle control signals.
 		}
 	}
 }
 
-func incrementing(add_number chan<-int, finished chan<- bool) {
-	for j := 0; j<1000000; j++ {
+func incrementing(add_number chan<- int, finished chan<- bool) {
+	for j := 0; j < 1000000; j++ {
 		add_number <- 1
 	}
 	//TODO: signal that the goroutine is finished
 }
 
 func decrementing(add_number chan<- int, finished chan<- bool) {
-	for j := 0; j<1000000; j++ {
+	for j := 0; j < 1000000; j++ {
 		add_number <- -1
 	}
 	//TODO: signal that the goroutine is finished
@@ -49,7 +49,7 @@ func main() {
 
 	// TODO: block on finished from both "worker" goroutines
 
-	control<-GetNumber
-	Println("The magic number is:", <- number)
-	control<-Exit
+	control <- GetNumber
+	Println("The magic number is:", <-number)
+	control <- Exit
 }
