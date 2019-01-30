ecf69f9efb34632f864757a2e2a6bdf62578222f
Bernhard Paus Græsdal <bernhardpg@gmail.com>
2019-01-30 10:05:45 +0100

    Complete assignment in Python

--- Part2/python/foo.py
+++ Part2/python/foo.py
@@ -1,21 +1,30 @@
 # Python 3.3.3 and 2.7.6
 # python fo.py
 
-from threading import Thread
+from threading import Thread, Lock
 
 # Potentially useful thing:
 #   In Python you "import" a global variable, instead of "export"ing it when you declare it
 #   (This is probably an effort to make you feel bad about typing the word "global")
+mutex = Lock()
 i = 0
 
 def incrementingFunction():
     global i
     # TODO: increment i 1_000_000 times
 
+    for j in range(1000000):
+        with mutex:
+            i += 1
+
+
 def decrementingFunction():
     global i
     # TODO: decrement i 1_000_000 times
 
+    for j in range(1000001):
+        with mutex:
+            i -= 1
 
 
 def main():
@@ -23,13 +32,13 @@ def main():
 
     incrementing = Thread(target = incrementingFunction, args = (),)
     decrementing = Thread(target = decrementingFunction, args = (),)
-    
-    # TODO: Start both threads
-    
+
+    incrementing.start()
+    decrementing.start()
+
     incrementing.join()
     decrementing.join()
-    
-    print("The magic number is %d" % (i))
 
+    print("The magic number is %d" % (i))
 
 main()
